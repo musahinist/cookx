c4bf49172b03b004014e4cbfcc01359ab78a8fa1
musahinist <sahinmusa1@gmail.com>
2021-06-12 22:06:24 +0300

    fortune wheel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cookx/store/recipe/recipe_store.dart';
 import 'package:cookx/view/page/app_navigator.dart';
 import 'package:cookx/view/page/fotune_whell.dart';
@@ -44,53 +46,139 @@ class Wheel extends StatefulWidget {
 
 class _WheelState extends State<Wheel> with SingleTickerProviderStateMixin {
   late AnimationController animationController;
-  late Animation<double> rotateY;
+  ScrollController ctrl = ScrollController();
+  late Animation<double> rotate;
+  final Duration rotationDuration = const Duration(seconds: 10);
+  Curve rotateCurve = Curves.bounceOut;
+  final Tween<double> _rotationTween = Tween(begin: 0, end: 1);
+  int rotateMargin = 0;
+  double radius = 100;
+  int count = 8;
+
   @override
   initState() {
     super.initState();
-    animationController = new AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    )..repeat();
+    setAnimation();
+  }
 
-    rotateY = new Tween<double>(
-      begin: .0,
-      end: 1.0,
-    ).animate(new CurvedAnimation(
+  setAnimation() {
+    animationController = AnimationController(
+      duration: rotationDuration,
+      vsync: this,
+    );
+    // animationController.repeat();
+
+    rotate = _rotationTween.animate(new CurvedAnimation(
       parent: animationController,
-      curve: Curves.easeIn,
-    ));
+      curve: rotateCurve,
+    ))
+      ..addListener(() {
+        setState(() {});
+        if (animationController.isCompleted) {
+          //  animationController.reset();
+        }
+      });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          rotateMargin = math.Random().nextInt(count);
+          rotateCurve = Curves.easeOut;
+          if (animationController.isCompleted) {
+            animationController.reset();
+            animationController.forward();
+          } else if (animationController.isDismissed) {
+            animationController.forward();
+          }
+        },
+      ),
       body: Center(
-        child: RotatedBox(
-          quarterTurns: -1,
-          child: ListWheelScrollView.useDelegate(
-            physics: FixedExtentScrollPhysics(),
-            itemExtent: 350, perspective: 0.01,
-            //  useMagnifier: true,
-            diameterRatio: 1, renderChildrenOutsideViewport: true,
-            squeeze: 2,
-            clipBehavior: Clip.none,
-            offAxisFraction: -.5, //squeeze: 6, // perspective: 0.0099,
-            //  renderChildrenOutsideViewport: true,
-            //  perspective: 0.009,
-            //  magnification: 1.3,
-            childDelegate: ListWheelChildLoopingListDelegate(
-              children: List<Widget>.generate(
-                10,
-                (index) => RotatedBox(
-                    quarterTurns: 1,
-                    child:
-                        CircleAvatar(radius: 100, child: Text('${index + 1}'))),
+          child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Transform.rotate(
+              angle: rotate.value * 8 * math.pi +
+                  rotateMargin * 2 * math.pi / count,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      shape: BoxShape.circle,
+                      // image: DecorationImage(
+                      //   image: NetworkImage(
+                      //       'https://cdn.imgbin.com/16/6/7/imgbin-template-game-show-wheel-others-7rzmirYGArZp1c3MEGBSGvn8d.jpg'),
+                      // ),
+                    ),
+                    height: 2 * radius,
+                    width: 2 * radius,
+                  ),
+                  ...List.generate(
+                    count,
+                    (i) => Transform(
+                      transform: Matrix4.identity()
+                        ..translate(
+                            (radius - 20) +
+                                (rotate.value * radius * 1.3) *
+                                    cos(-i * 2 * math.pi / count),
+                            (radius - 20) +
+                                (rotate.value * radius * 1.3) *
+                                    sin(-i * 2 * math.pi / count)),
+                      child: Profile(
+                        rotate: rotate,
+                        rotateMargin: rotateMargin * 1.0,
+                        index: i,
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
-        ),
-      ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(rotateMargin.toString()),
+          )
+        ],
+      )),
     );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile(
+      {Key? key,
+      required this.rotate,
+      required this.rotateMargin,
+      required this.index})
+      : super(key: key);
+
+  final Animation<double> rotate;
+  final double rotateMargin;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+        angle: -rotate.value * 8 * math.pi - rotateMargin * 2 * math.pi / 8,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            CircleAvatar(
+              radius: 20,
+              // backgroundImage: NetworkImage(
+              //     'https://www.eaclinic.co.uk/wp-content/uploads/2019/01/woman-face-eyes-1000x1000.jpg'),
+            ),
+            Text(
+              index.toString(),
+              style: TextStyle(fontSize: 24),
+            )
+          ],
+        ));
   }
 }
